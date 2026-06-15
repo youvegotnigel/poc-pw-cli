68f6c17ee6e8fdfdb3810c6160b5fbb5deee58d3
Nigel <nigeldave19@gmail.com>
2026-06-15 23:21:10 +0530

    Create checkout.page.ts

---
+++ src/pages/checkout.page.ts
@@ -0,0 +1,33 @@
+import { type Page, type Locator } from '@playwright/test';
+import { BasePage } from './base.page';
+
+export class CheckoutPage extends BasePage {
+  readonly subtotal: Locator;
+  readonly total: Locator;
+  readonly confirmationHeader: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.subtotal = page.getByTestId('subtotal-label');
+    this.total = page.getByTestId('total-label');
+    this.confirmationHeader = page.getByTestId('complete-header');
+  }
+
+  async fillInfo(firstName: string, lastName: string, postalCode: string): Promise<void> {
+    await this.page.getByTestId('firstName').fill(firstName);
+    await this.page.getByTestId('lastName').fill(lastName);
+    await this.page.getByTestId('postalCode').fill(postalCode);
+  }
+
+  async continue(): Promise<void> {
+    await this.page.getByTestId('continue').click();
+  }
+
+  async finish(): Promise<void> {
+    await this.page.getByTestId('finish').click();
+  }
+
+  async backHome(): Promise<void> {
+    await this.page.getByTestId('back-to-products').click();
+  }
+}
